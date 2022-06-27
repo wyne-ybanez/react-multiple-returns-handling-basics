b3c243f840e1e06a5742685d6d530bd518705824
Wyne Ybanez <wyneybanez@gmail.com>
2022-06-27 15:39:25 +0100

    Add fetch url with useEffect, Add cases for loading, error and user

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react'
-function App() {
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => {
+        console.log(user)
+
+        const userLogin = user.login
+        setUser(userLogin)
+        setIsLoading(false) // set is loading to false once we have the data we need
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    )
+  }
+
+  // Default response is that we are getting the user
   return (
-    <div className="container">
-      <h2>Empty Canvas</h2>
+    <div>
+      <h1>{user}</h1>
     </div>
   )
 }
