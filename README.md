fd272a47314ba9a7ff5d89291a161b0a30d87aff
Wyne Ybanez <wyneybanez@gmail.com>
2022-06-27 15:05:36 +0100

    Add project initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Multiple Returns Basics
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function App() {
+  return (
+    <div className="container">
+      <h2>Empty Canvas</h2>
+    </div>
+  )
+}
+
+export default App
